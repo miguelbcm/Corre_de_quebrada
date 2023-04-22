84b96c43328e491133600911314d12e7e70c25cc
Miguel Barbosa Cavalcante Mendes <miguelbarbosa763@gmail.com>
2023-04-21 22:37:41 -0300

    enviando arquivo

---
+++ corre.js
@@ -0,0 +1,85 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal(' #logo', {duration: 2000 });
+
+sr.reveal('#never',{duration: 2000 });
+
+sr.reveal('.home',{duration: 2000 });
+
+sr.reveal('.quem_somos',{duration: 2000 });
+
+sr.reveal('.aulas',{duration: 2000 });
+
+sr.reveal('.matricule',{duration: 2000 });
+
+sr.reveal(' #imagem_section', {duration: 2000 });
+
+sr.reveal(' #titulo_ingles', {duration: 2000 });
+
+sr.reveal(' #texto', {duration: 2000 });
+
+sr.reveal(' #comece', {duration: 2000 })
+
+sr.reveal(' .texto2', {duration: 2000 })
+
+sr.reveal(' #crianca', {duration: 2000 });
+
+sr.reveal(' #texto_crianca', {duration: 2000 });
+
+sr.reveal(' #terceira_idade', {duration: 2000 });
+
+sr.reveal(' #texto_idade', {duration: 2000 });
+
+sr.reveal(' #presencial', {duration: 2000 });
+
+sr.reveal(' #texto_presencial', {duration: 2000 });
+
+sr.reveal(' #online', {duration: 2000 });
+
+sr.reveal(' #texto_online', {duration: 2000 });
+
+sr.reveal(' #our_teacher', {duration: 2000 });
+
+sr.reveal(' #imagem_teacher', {duration: 2000 });
+
+sr.reveal(' #bandeiras', {duration: 2000 });
+
+sr.reveal(' #texto_teacher', {duration: 2000 });
+
+sr.reveal(' #titulo_h3', {duration: 2000 });
+
+sr.reveal(' #mapa', {duration: 2000 });
+
+sr.reveal(' #logof', {duration: 2000 });
+
+sr.reveal(' .coluna1', {duration: 2000 });
+
+sr.reveal(' .coluna2', {duration: 2000 });
+
+sr.reveal(' .coluna3', {duration: 2000 });
+
+sr.reveal(' .coluna4', {duration: 2000 });
+
+sr.reveal(' #titulo_contato', {duration: 2000 });
+
+sr.reveal(' .icones1', {duration: 2000 });
+
+sr.reveal(' .icones2', {duration: 2000 });
+
+sr.reveal(' .icones3', {duration: 2000 });
+
+sr.reveal(' .texto_icones1', {duration: 2000 });
+
+sr.reveal(' .texto_icones2', {duration: 2000 });
+
+sr.reveal(' .texto_icones3', {duration: 2000 });
+
+
+
+
+
+
+
+
+
+
